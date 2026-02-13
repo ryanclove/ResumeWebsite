b5338abe48f968e29de9a116c7a66f37fa6f626d
Ryan Coslove <ryancoslove@gmail.com>
2026-02-13 12:03:59 -0500

    more testimonials and rearragned

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -406,14 +406,26 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
+      name: 'Claudia A. (Former 16s Player)',
+      text: 'Thank you so much for this season and everything you\'ve done for us this season! I had so much fun and learned so much from you.',
+    },
+    {
       name: 'Lora Ann H. (Mother of 16s Player)',
-      text: "You're the best coach [player name] has ever had. She's improved more since November than she has in any other full season... You've given her confidence in herself and her abilities, which allows her to try more things on the court- which can lead to mistakes. But, you've given her the confidence to be able to make mistakes, which is one of your most important roles (IMO). I've talked to pretty much every parent and all of us have the same feeling about you and this team. We're happy the girls are getting along, happy that you have a better understanding of teenagers than many adults, happy with your positivity, happy with you setting high expectations of their behavior and just all around happy with the team.",
+      text: "You're the best coach Lily Mae has ever had. She's improved more since November than she has in any other full season... You've given her confidence in herself and her abilities, which allows her to try more things on the court- which can lead to mistakes. But, you've given her the confidence to be able to make mistakes, which is one of your most important roles (IMO). I've talked to pretty much every parent and all of us have the same feeling about you and this team. We're happy the girls are getting along, happy that you have a better understanding of teenagers than many adults, happy with your positivity, happy with you setting high expectations of their behavior and just all around happy with the team.",
       //image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
+      name: 'Leah W. (Former 17s Player)',
+      text: 'Since day one when I met you at tryouts you made the space so welcoming and sweet. I wasn\'t afraid to make mistakes and was ready to learn. Thank you for everything!',
+    },
+    {
       name: 'Stacie B. (Mother of 16s Player)',
-      text: " I've seen such a positive change in [player name] in just a short time...  [You've shown] a deep level of self awareness and maturity beyond your years. One team one fight!",
+      text: " I've seen such a positive change in Addie in just a short time...  [You've shown] a deep level of self awareness and maturity beyond your years. One team one fight!",
       //image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    },
+    {
+      name: 'Rachel B. (Former 17s Player)',
+      text: 'Thank you for being an awesome coach. You made me feel like I was heard. Thank you for having a positive impact on my life and for being so understanding.',
     },
     {
       name: 'Jennie B. (Mother of 16s Player)',
@@ -422,11 +434,19 @@ export const testimonial: TestimonialSection = {
     },
     {
       name: 'Sherry F. (Mother of 17s Player)',
-      text: 'Thanks for a great season! [player name] says it was her favorite Vienna year!',
+      text: 'Thanks for a great season! Emily says it was her favorite Vienna year!',
+    },
+    {
+      name: 'Clara A. (Former 16s Player)',
+      text: 'Thank you for making this season fun and full of surprises and helping us all get better! This season was the best and I don\'t know what we will do with a different coach. THank you for making everything a celebration, spending time and money on us. I\'ve learned so much this season',
     },
     {
       name: 'Dolores B. (Mother of 17s Player)',
       text: 'Thank you for being such a good coach and human 😀. Vienna Elite is lucky to have you.',
+    },
+    {
+      name: 'Neema K. (Former 16s Player)',
+      text: 'Thank you so much for being our coach this year and helping me develop my skills. I couldn\'t be where I am without you. Also thank you for being someone who we can talk to and confide in.'
     },
   ],
 };
