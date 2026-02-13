a23d5f95800561f4c45ac1ed9354dcbcf7a2b2ae
Ryan Coslove <ryancoslove@gmail.com>
2026-02-12 21:33:29 -0500

    bottom arrow was invisible in light mode

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: FC = memo(() => (
       <a
         className="rounded-full bg-neutral-100 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
         href={`/#${SectionId.Hero}`}>
-        <ChevronUpIcon className="h-6 w-6 bg-transparent sm:h-8 sm:w-8" />
+        <ChevronUpIcon className="h-6 w-6 bg-transparent text-black sm:h-8 sm:w-8" />
       </a>
     </div>
     <div className="flex flex-col items-center gap-y-6">
