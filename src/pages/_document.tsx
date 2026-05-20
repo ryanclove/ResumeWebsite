b1edff7086b89cddcc8d66c0d74f5812e78bd718
Ryan Coslove <ryancoslove@gmail.com>
2026-05-20 17:45:56 -0400

    improve preview stuff on google searches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ export default function Document() {
         <meta content="notranslate" name="google" />
         <meta name="google-site-verification" content="FU45Mtwq_G5B8VfzRZVgpTFhqCViCBgKMO7Mn05_T98" />
         <meta property="og:image" content="https://coachryantutu.vercel.app/previewimage.jpg" />
+        <meta property="og:title" content="Ryan Coslove - Volleyball Coach" />
+        <meta property="og:description" content="DMV-based volleyball coach." />
+        <meta name="description" content="Ryan Coslove is a DMV-based Volleyball Coach." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="bg-gray-800 dark:bg-black text-white m-0 p-0">
